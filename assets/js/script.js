836cd4d759fa1d507d3b1931eeecad6b7916b75a
Kurtis Doute <kcd257@yahoo.com>
2021-12-15 21:27:27 -0800

    Cleaned the code to make it more traversable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 var weatherData;
+var trailsData;
 
 var imgEl = document.querySelectorAll("img");
 var dateEl = document.getElementsByClassName("Date");
@@ -8,15 +9,14 @@ var minEl = document.getElementsByClassName("Min");
 var windEl = document.getElementsByClassName("Wind");
 var weatherMainEl = document.getElementsByClassName("WeatherMain");
 var weatherDesEl = document.getElementsByClassName("WeatherDes");
+var menuList = document.querySelector(".menu");
+var searchBtn = document.querySelector("#searchBtn");
+var hiddenObj = document.querySelectorAll(".hidden");
 
-//When the search button is clicked the functions will start.. Add location to the API to give weather of state
-function getTrails(){
-    displayTrails();
-    
-}
-// Input: Need Latitude, Longitude, API key (https://openweathermap.org/api/one-call-api)
+searchBtn.addEventListener("click", displayTrails);
+
 function getWeatherData(latitude, longitude) {
-    // TODO: get actual OPEN WEATHER API url and key
+
     var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude +"&lon=" + longitude +"&exclude=current,minutely,hourly&appid=31ed1d78ece05a26dbb0c6020e7b32b5&units=imperial";
     fetch(apiUrl)
         .then(function (response) {
@@ -51,14 +51,6 @@ function renderWeather() {
     }
 };
 
-function saveDataLocal() {
-    localStorage.setItem("TESTING", JSON.stringify(weatherData));
-}
-
-function loadDataLocal() {
-    weatherData = JSON.parse(localStorage.getItem("TESTING"));
-}
-
 function unixConversion(unix) {
     var date = new Date(unix * 1000);
     var temp = date.toDateString();
@@ -66,91 +58,49 @@ function unixConversion(unix) {
     return temp;
 }
 
-// Start of trails function
-//When the user gives the state code he needs to be able to pull a list of trails in the given state 
-
-
-var searchBtn = document.querySelector("#searchBtn");
-searchBtn.addEventListener("click", getTrails);
-var menuList = document.querySelector(".menu");
 
 function displayTrails(){
     var selectState = document.querySelector("#selectState");
     var state = selectState.options[selectState.selectedIndex].value  //because options is an array,selected index is the index of the one we have currently selelcted
-
-   
-    var apiUrl = "https://developer.nps.gov/api/v1/places?statecode=" + state + "&limit=10&q=trails&api_key=WdgBOclP1YDr6ZIL0vXfInjZRVwmb8VjKrcvwpoZ"
-
+    
+    var apiUrl = "https://developer.nps.gov/api/v1/places?statecode=" + state + "&limit=10&q=trails&api_key=WdgBOclP1YDr6ZIL0vXfInjZRVwmb8VjKrcvwpoZ";
+    
+    unHide();
 
     fetch(apiUrl)
     .then(function (response) {
-        if (response.ok)
-        response.json().then(function (data) {
-            trailsData = data;
-            console.log(data);
-            updateUI();
-            // setImage();
-                    // var menuList = document.querySelector(".menu");
-                    for (var i = 0; i < data.data.length; i++) {
-                        var item = document.createElement("li");
-                        item.dataset.latitude = data.data[i].latitude;
-                        item.dataset.longitude = data.data[i].longitude;
-                        item.textContent = data.data[i].title;
-                        item.dataset.text = data.data[i].bodyText;
-                        item.dataset.image = data.data[i].images[0].url;
-                        item.addEventListener("click", function (event) {
-                            getWeatherData(event.target.dataset.latitude, event.target.dataset.longitude); 
-                            var trailDescription = document.querySelector("#trailDescription");
-                            trailDescription.innerHTML = event.target.dataset.text; //dataset stores extra information  
-                            var koolAidMan = document.getElementById("koolAidMan");
-                            koolAidMan.setAttribute("src", event.target.dataset.image);
-                        })
-                        
-                        
-                        
-                        
-                        
-                        
-                        menuList.appendChild(item);
+        if (response.ok) {
+            response.json().then(function (data) {
+                trailsData = data;
+                console.log(data);
+                updateUI();
+                for (var i = 0; i < data.data.length; i++) {
+                    var item = document.createElement("li");
+                    item.dataset.latitude = data.data[i].latitude;
+                    item.dataset.longitude = data.data[i].longitude;
+                    item.textContent = data.data[i].title;
+                    item.dataset.text = data.data[i].bodyText;
+                    item.dataset.image = data.data[i].images[0].url;
+                    item.addEventListener("click", function setDescription(event) {
+                        getWeatherData(event.target.dataset.latitude, event.target.dataset.longitude); 
+                        var trailDescription = document.querySelector("#trailDescription");
+                        trailDescription.innerHTML = event.target.dataset.text; //dataset stores extra information  
+                        var koolAidMan = document.getElementById("koolAidMan");
+                        koolAidMan.setAttribute("src", event.target.dataset.image);
+                    });
+
+                    menuList.appendChild(item);
                 }
             });
+        }
     });
-}
+};
 
 function updateUI() {
-    console.log("Sup")
     menuList.replaceChildren();
 }
-
-
-
-
-
-// var trailEl = document.querySelector('ul');
-var trailsData;
-
-
-
-
-
-
-// function showTitle() {
-//     for (var i = 0; i < trailsData.length; i++) {
-//         var trails = trailsData.data.title;
-//         trailEl
-//         console.log(trailsData);
-//     };
-// };
-
-// getTrailsData();
-// showTitle();
-
-
-// Ajane's adding image
-// This is how we access the URL of the image we want
-// trailsData.data[0].images[0].url
-// var koolAidMan = document.getElementById("koolAidMan");
-// function setImage() {
-//     koolAidMan.src = trailsData.data[0].images[0].url;
-// }
-
+function unHide() {
+    for (var i = hiddenObj.length-1; i >= 0; i--) {
+        hiddenObj[i].classList.remove("hidden");
+    }
+}
